2523201dd019e977568ac245137e3a4dbfc16f92
hmlm90020798-alt <hmlm.90020798@gmail.com>
2026-04-05 18:17:28 +0100

    corrigir vite config

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  base: '/hm-work-kit/',,
+  base: '/hm-work-kit/',
   plugins: [
     react(),
     VitePWA({
